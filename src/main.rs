d0a9faab9d0220a1581b8676565757caa2a25f56
Juan José González <juanjosegzl@gmail.com>
2017-09-17 15:35:38 -0500

    Arena is the World entity of the game
    it stores all entities that lives in the game, runs and render
    
    Simplify importing using piston_window crate instead

--- src/main.rs
+++ src/main.rs
@@ -3,43 +3,31 @@
 
 //! A vikings dodgeball game with axes a la Rust
 
-extern crate piston;
-extern crate glutin_window;
-extern crate graphics;
+extern crate piston_window;
 extern crate opengl_graphics;
 
-use piston::window::WindowSettings;
-use glutin_window::GlutinWindow;
-use piston::input::RenderEvent;
-use piston::event_loop:: {Events, EventSettings, EventLoop};
-use opengl_graphics::{OpenGL, GlGraphics};
-use graphics::{clear};
-use drawing::color::BLACK;
+use piston_window::{OpenGL, PistonWindow, WindowSettings};
+use opengl_graphics::GlGraphics;
 
+use arena::Arena;
 mod drawing;
+mod arena;
 
 fn main() {
     let opengl = OpenGL::V3_2;
 
-  /* Tell window backend which OpenGL version to use */
+    /* Tell window backend which OpenGL version to use */
     let settings = WindowSettings::new("Vikings Dodgeaxe", [255, 255])
         .opengl(opengl)
         .exit_on_esc(true);
 
-    let mut window: GlutinWindow = settings.build()
+    let mut window: PistonWindow = settings.build()
         .expect("Could not create main window");
-
-    let mut events = Events::new(EventSettings::new());
     let mut gl = GlGraphics::new(opengl);
 
-    while let Some(e) = events.next(&mut window) {
-        if let Some(args) = e.render_args() {
-          /* Get rectangular area inside the frame
-           * buffer and draw on it */
-          gl.draw(args.viewport(), |c, g| {
-            clear(BLACK, g);
-          });
-        }
-    }
+    let mut arena = Arena::new();
+
+    // main loop
+    arena.run(&mut window, &mut gl);
 }
 
